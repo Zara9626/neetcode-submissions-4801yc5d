6cc4cf31c28d635bc0d051ee20391c06e6df4cd8
Zara9626 <63889471+Zara9626@users.noreply.github.com>
2026-04-23 13:31:35 -0700

    Add: maximum-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-subarray/submission-6.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curSum = 0;
+        int maxSum = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            curSum = Math.max(curSum, 0);
+            curSum += nums[i];
+            maxSum = Math.max(curSum, maxSum);
+
+        }
+        return maxSum;
+    }
+}
